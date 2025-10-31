7051e29b8555c2c514e51e8aebe4352c67b3aabc
Ian Nelson <ian@iannelson.uk>
2025-10-31 22:18:34 +0000

    hangfire

--- src/server/Hoard.Scheduler/Program.cs
+++ src/server/Hoard.Scheduler/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("HoardDatabase");
+
+builder.Services.AddHangfire(config => config
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(connectionString));
+
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+app.UseHangfireDashboard("/jobs");
+
+app.MapGet("/", () => "Hoard Scheduler running.");
+
+RecurringJob.AddOrUpdate(
+    "refresh-quotes",
+    () => Console.WriteLine("Refreshing quotes..."),
+    Cron.Minutely);
+
+app.Run();
