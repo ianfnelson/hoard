6603b4cf26f9a01e2c9cc2b54fefc505055d39ab
Ian Nelson <ian@loxodromic.systems>
2025-11-05 11:15:22 +0000

    feat: simpler prices backfill (#76)

--- src/server/Hoard.Core/DateOnlyHelper.cs
+++ src/server/Hoard.Core/DateOnlyHelper.cs
@@ -4,4 +4,6 @@ public static class DateOnlyHelper
 {
     public static DateOnly TodayLocal() => DateOnly.FromDateTime(DateTime.Now);
     public static DateOnly OneYearAgoLocal() => DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-1));
+
+    public static DateOnly EpochLocal() => new(2000, 1, 1);
 }
