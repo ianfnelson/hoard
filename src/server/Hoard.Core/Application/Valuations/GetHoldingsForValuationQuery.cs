64c64bc673bc215c7af32f4a6f1f4e537702e597
Ian Nelson <ian@loxodromic.systems>
2025-11-11 18:29:51 +0000

    refactor: more layer refactoring (#104)

--- src/server/Hoard.Core/Application/Valuations/GetHoldingsForValuationQuery.cs
+++ src/server/Hoard.Core/Application/Valuations/GetHoldingsForValuationQuery.cs
@@ -1,6 +1,19 @@
+using Hoard.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Hoard.Core.Application.Valuations;
 
-public class GetHoldingsForValuationQuery
+public record GetHoldingsForValuationQuery(DateOnly AsOfDate)
+    : IQuery<IReadOnlyList<int>>;
+
+public class GetHoldingsForValuationHandler(HoardContext context)
+    : IQueryHandler<GetHoldingsForValuationQuery, IReadOnlyList<int>>
 {
-    
+    public async Task<IReadOnlyList<int>> HandleAsync(GetHoldingsForValuationQuery query, CancellationToken ct = default)
+    {
+        return await context.Holdings
+            .Where(x => x.AsOfDate == query.AsOfDate)
+            .Select(x => x.Id)
+            .ToListAsync(ct);
+    }
 }
