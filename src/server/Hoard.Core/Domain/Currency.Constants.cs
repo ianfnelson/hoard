598d03e0ba55fadc1b815f87383864737f73e7c7
Ian Nelson <ian@iannelson.uk>
2025-11-15 19:51:10 +0000

    feat: Add Danish Krone

--- src/server/Hoard.Core/Domain/Currency.Constants.cs
+++ src/server/Hoard.Core/Domain/Currency.Constants.cs
@@ -7,4 +7,5 @@ public partial class Currency
     public const string Usd = "USD";
     public const string Eur = "EUR";
     public const string Jpy = "JPY";
+    public const string Dkk = "DKK";
 }
