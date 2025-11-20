42d46ee6a0d1582e3f4bf68783a199229e8a602b
Ian Nelson <ian@loxodromic.systems>
2025-11-20 21:46:04 +0000

    feat: cashflows with backfill (#140)

---
+++ src/server/Hoard.Api/Controllers/CashflowsController.cs
@@ -0,0 +1,22 @@
+using Hoard.Core.Application;
+using Hoard.Core.Application.Cashflow;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hoard.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public class CashflowsController(IMediator mediator, ILogger<CashflowsController> logger) : ControllerBase
+{
+    [HttpPost("backfill")]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    public async Task<IActionResult> BackfillHoldingsAsync()
+    {
+        logger.LogInformation("Received request to backfill cashflow.");
+        
+        await mediator.SendAsync(new TriggerBackfillCashflowCommand(Guid.NewGuid()));
+        
+        return Accepted(new { message = "Backfill cashflow triggered." });
+    }
+}
---
+++ src/server/Hoard.Core/Data/Migrations/20251120204949_Cashflowfin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hoard.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cashflowfin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cashflow",
+                columns: table => new
+                {
+                    CashflowId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountId = table.Column<int>(type: "int", nullable: false),
+                    InstrumentId = table.Column<int>(type: "int", nullable: true),
+                    TransactionId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedUtc = table.Column<DateTime>(type: "datetime2(3)", nullable: false, defaultValueSql: "SYSUTCDATETIME()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cashflow", x => x.CashflowId);
+                    table.ForeignKey(
+                        name: "FK_Cashflow_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "AccountId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Cashflow_Instrument_InstrumentId",
+                        column: x => x.InstrumentId,
+                        principalTable: "Instrument",
+                        principalColumn: "InstrumentId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Cashflow_Transaction_TransactionId",
+                        column: x => x.TransactionId,
+                        principalTable: "Transaction",
+                        principalColumn: "TransactionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cashflow_AccountId_Date",
+                table: "Cashflow",
+                columns: new[] { "AccountId", "Date" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cashflow_InstrumentId_Date",
+                table: "Cashflow",
+                columns: new[] { "InstrumentId", "Date" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cashflow_TransactionId",
+                table: "Cashflow",
+                column: "TransactionId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cashflow");
+        }
+    }
+}
