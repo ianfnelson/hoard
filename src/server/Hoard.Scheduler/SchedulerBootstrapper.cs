f098bc72ad28b48ff7123a3179860a0c05accc2a
Ian Nelson <ian@loxodromic.systems>
2025-11-21 19:13:54 +0000

    chore: schedule change (#141)

--- src/server/Hoard.Scheduler/SchedulerBootstrapper.cs
+++ src/server/Hoard.Scheduler/SchedulerBootstrapper.cs
@@ -4,6 +4,7 @@ using Hoard.Core.Application;
 using Hoard.Core.Application.Holdings;
 using Hoard.Core.Application.Prices;
 using Hoard.Core.Application.Quotes;
+using Hoard.Core.Application.Valuations;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -35,6 +36,7 @@ public class SchedulerBootstrapper : IHostedService
         RegisterCalculateHoldings();
         RegisterRefreshQuotes();
         RegisterRefreshPrices();
+        RegisterCalculateValuations();
         
         return Task.CompletedTask;
     }
@@ -43,7 +45,7 @@ public class SchedulerBootstrapper : IHostedService
     {
         _recurring.AddOrUpdate(
             "calculate-holdings",
-            () => TriggerCalculateHoldings(),
+            () => TriggerCalculateHoldingsCommand(),
             "0 2 * * *" // nightly at 2am
         );
     }
@@ -52,7 +54,7 @@ public class SchedulerBootstrapper : IHostedService
     {
         _recurring.AddOrUpdate(
             "refresh-quotes",
-            () => SendRefreshQuotesCommand(),
+            () => TriggerRefreshQuotesCommand(),
             "*/2 8-17 * * 1-5" // every two minutes during UK trading hours
         );
     }
@@ -61,8 +63,17 @@ public class SchedulerBootstrapper : IHostedService
     {
         _recurring.AddOrUpdate(
             "refresh-prices",
-            () => SendRefreshPricesCommand(),
-            "0 17,22,23 * * 1-5" // weekdays at 17:00, 22:00, 23:00
+            () => TriggerRefreshPricesCommand(),
+            "45 17,21,22 * * 1-5" // weekdays at 17:45, 21:45, 22:45
+        );
+    }
+    
+    private void RegisterCalculateValuations()
+    {
+        _recurring.AddOrUpdate(
+            "refresh-prices",
+            () => TriggerCalculateValuationsCommand(),
+            "15 23 * * 1-5" // weekdays 23:15
         );
     }
     
@@ -73,7 +84,7 @@ public class SchedulerBootstrapper : IHostedService
     }
 
     // ReSharper disable once MemberCanBePrivate.Global public required for Hangfire background jobs
-    public async Task TriggerCalculateHoldings()
+    public async Task TriggerCalculateHoldingsCommand()
     {
         _logger.LogInformation("Triggering Calculate Holdings");
 
@@ -83,7 +94,7 @@ public class SchedulerBootstrapper : IHostedService
     }
 
     // ReSharper disable once MemberCanBePrivate.Global public required for Hangfire background jobs
-    public async Task SendRefreshQuotesCommand()
+    public async Task TriggerRefreshQuotesCommand()
     {
         _logger.LogInformation("Triggering Refresh Quotes");
 
@@ -93,11 +104,21 @@ public class SchedulerBootstrapper : IHostedService
     }
 
     // ReSharper disable once MemberCanBePrivate.Global public required for Hangfire background jobs
-    public async Task SendRefreshPricesCommand()
+    public async Task TriggerRefreshPricesCommand()
     {
         _logger.LogInformation("Triggering Refresh Prices");
 
         var command = new TriggerRefreshPricesCommand(Guid.NewGuid(), DateOnlyHelper.TodayLocal());
+        
+        await _mediator.SendAsync(command);
+    }
+    
+    // ReSharper disable once MemberCanBePrivate.Global public required for Hangfire background jobs
+    public async Task TriggerCalculateValuationsCommand()
+    {
+        _logger.LogInformation("Triggering Calculate Valuations");
+
+        var command = new TriggerCalculateValuationsCommand(Guid.NewGuid(), DateOnlyHelper.TodayLocal());
         
         await _mediator.SendAsync(command);
     }
