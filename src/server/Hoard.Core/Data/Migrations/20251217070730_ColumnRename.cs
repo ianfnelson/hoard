25111f3a204285b2849b0cf78e225d5ce8a98c26
Ian Nelson <ian@iannelson.uk>
2025-12-17 07:08:45 +0000

    fix: migration - column rename

---
+++ src/server/Hoard.Core/Data/Migrations/20251217070730_ColumnRename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hoard.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PortfolioPerformanceId",
+                table: "PositionPerformance",
+                newName: "PositionPerformanceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PositionPerformanceId",
+                table: "PositionPerformance",
+                newName: "PortfolioPerformanceId");
+        }
+    }
+}
