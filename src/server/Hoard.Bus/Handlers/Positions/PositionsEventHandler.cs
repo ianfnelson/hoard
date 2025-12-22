7f93621371aefc6c9e64163c24c11ded9e40672d
Ian Nelson <ian@iannelson.uk>
2025-12-22 20:57:53 +0000

    feat: positions triggers performance

--- src/server/Hoard.Bus/Handlers/Positions/PositionsEventHandler.cs
+++ src/server/Hoard.Bus/Handlers/Positions/PositionsEventHandler.cs
@@ -1,4 +1,5 @@
 using Hoard.Core.Application;
+using Hoard.Core.Application.Performance;
 using Hoard.Core.Application.Positions;
 using Hoard.Messages.Positions;
 using Rebus.Handlers;
@@ -6,12 +7,20 @@ using Rebus.Handlers;
 namespace Hoard.Bus.Handlers.Positions;
 
 public class PositionsEventHandler(IMediator mediator)
-    : IHandleMessages<CalculatePositionsBusCommand>
+    : 
+        IHandleMessages<CalculatePositionsBusCommand>,
+        IHandleMessages<PositionsCalculatedEvent>
 {
     public async Task Handle(CalculatePositionsBusCommand message)
     {
         var appCommand = new ProcessCalculatePositionsCommand(message.PositionsRunId, message.PipelineMode);
         
+        await mediator.SendAsync(appCommand);
+    }
+
+    public async Task Handle(PositionsCalculatedEvent message)
+    {
+        var appCommand = new TriggerCalculatePerformanceCommand(null, message.PipelineMode);
         await mediator.SendAsync(appCommand);
     }
 }
