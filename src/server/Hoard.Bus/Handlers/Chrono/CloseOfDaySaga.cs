cb2992903690716b896115abd9c4168054028298
Ian Nelson <ian@loxodromic.systems>
2025-12-22 16:31:44 +0000

    refactor: close of day saga (#248)

--- src/server/Hoard.Bus/Handlers/Chrono/CloseOfDaySaga.cs
+++ src/server/Hoard.Bus/Handlers/Chrono/CloseOfDaySaga.cs
@@ -56,33 +56,9 @@ public class CloseOfDaySaga(IMediator mediator, ILogger<CloseOfDaySaga> logger)
         
         logger.LogInformation("CloseOfDay {ChronoRunId}: Starting {PipelineMode} for {AsOfDate}", 
             chronoRunId, pipelineMode, asOfDate.ToIsoDateString());
-        
-        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Holdings", Data.ChronoRunId);
-        var triggerHoldingsCommand =
-            new TriggerBackfillHoldingsCommand(Data.RebuildStartDate, Data.Tomorrow, pipelineMode);
-        Data.HoldingsRunId = triggerHoldingsCommand.HoldingsRunId;
-        await mediator.SendAsync(triggerHoldingsCommand);
-        
-        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Prices", Data.ChronoRunId);
-        var triggerPricesCommand =
-            new TriggerRefreshPricesCommand(null, Data.RebuildStartDate, Data.Today, pipelineMode);
-        Data.PricesRunId = triggerPricesCommand.PricesRunId;
-        await mediator.SendAsync(triggerPricesCommand);
-    }
 
-    public async Task Handle(PricesRefreshedEvent message)
-    {
-        Data.PricesRefreshed = true;
-        logger.LogInformation("CloseOfDay {ChronoRunId}: Prices refreshed.", Data.ChronoRunId);
-
-        if (Data.HoldingsCalculated && !Data.ValuationsCalculated)
-        {
-            logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Valuations (prices now available)", Data.ChronoRunId);
-            var triggerValuationsCommand =
-                new TriggerBackfillValuationsCommand(null, Data.RebuildStartDate, Data.Tomorrow, Data.PipelineMode);
-            Data.ValuationsRunId = triggerValuationsCommand.ValuationsRunId;
-            await mediator.SendAsync(triggerValuationsCommand);
-        }
+        await DispatchPrices();
+        await DispatchHoldings();
     }
 
     public async Task Handle(HoldingsBackfilledEvent message)
@@ -92,46 +68,24 @@ public class CloseOfDaySaga(IMediator mediator, ILogger<CloseOfDaySaga> logger)
 
         if (!Data.PositionsCalculated)
         {
-            logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Positions", Data.ChronoRunId);
-            
-            var positionsCommand = new TriggerCalculatePositionsCommand(Data.PipelineMode);
-            Data.PositionsRunId = positionsCommand.PositionsRunId;
-            
-            await mediator.SendAsync(positionsCommand);
+            await DispatchPositions();
         }
-    }
 
-    public async Task Handle(PositionsCalculatedEvent message)
-    {
-        Data.PositionsCalculated = true;
-        logger.LogInformation("CloseOfDay {ChronoRunId}: Positions calculated.", Data.ChronoRunId);
-        
-        if (Data.PricesRefreshed &&
-            Data.HoldingsCalculated &&
-            !Data.ValuationsCalculated)
+        if (Data.PricesRefreshed && !Data.ValuationsCalculated)
         {
-            logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Valuations", Data.ChronoRunId);
-            var triggerValuationsCommand =
-                new TriggerBackfillValuationsCommand(null, Data.RebuildStartDate, Data.Tomorrow, Data.PipelineMode);
-            Data.ValuationsRunId = triggerValuationsCommand.ValuationsRunId;
-            await mediator.SendAsync(triggerValuationsCommand);
+            await DispatchValuations();
         }
     }
 
-    public async Task Handle(ValuationsBackfilledEvent message)
+    public async Task Handle(PricesRefreshedEvent message)
     {
-        Data.ValuationsCalculated = true;
-        logger.LogInformation("CloseOfDay {ChronoRunId}: Valuations calculated.", Data.ChronoRunId);
-        
-        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Performance", Data.ChronoRunId);
-        var triggerPerformanceCommand = new TriggerCalculatePerformanceCommand(null, Data.PipelineMode);
-        Data.PerformanceRunId = triggerPerformanceCommand.PerformanceRunId;
-        await mediator.SendAsync(triggerPerformanceCommand);
-        
-        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Snapshots", Data.ChronoRunId);
-        var triggerSnapshotsCommand = new TriggerCalculateSnapshotsCommand(Data.Today.Year, Data.PipelineMode);
-        Data.SnapshotsRunId = triggerSnapshotsCommand.SnapshotsRunId;
-        await mediator.SendAsync(triggerSnapshotsCommand);
+        Data.PricesRefreshed = true;
+        logger.LogInformation("CloseOfDay {ChronoRunId}: Prices refreshed.", Data.ChronoRunId);
+
+        if (Data.HoldingsCalculated && !Data.ValuationsCalculated)
+        {
+            await DispatchValuations();
+        }
     }
 
     public Task Handle(PerformanceCalculatedEvent message)
@@ -146,7 +100,31 @@ public class CloseOfDaySaga(IMediator mediator, ILogger<CloseOfDaySaga> logger)
         
         return Task.CompletedTask;
     }
-    
+
+    public async Task Handle(PositionsCalculatedEvent message)
+    {
+        Data.PositionsCalculated = true;
+        logger.LogInformation("CloseOfDay {ChronoRunId}: Positions calculated.", Data.ChronoRunId);
+        
+        if (Data.ValuationsCalculated && !Data.PerformanceCalculated)
+        {
+            await DispatchPerformance();
+        }
+    }
+
+    public async Task Handle(ValuationsBackfilledEvent message)
+    {
+        Data.ValuationsCalculated = true;
+        logger.LogInformation("CloseOfDay {ChronoRunId}: Valuations calculated.", Data.ChronoRunId);
+
+        if (Data.PositionsCalculated && !Data.PerformanceCalculated)
+        {
+            await DispatchPerformance();
+        }
+        
+        await DispatchSnapshots();
+    }
+
     public Task Handle(SnapshotsCalculatedEvent message)
     {
         Data.SnapshotsCalculated = true;
@@ -159,7 +137,58 @@ public class CloseOfDaySaga(IMediator mediator, ILogger<CloseOfDaySaga> logger)
         
         return Task.CompletedTask;
     }
-    
+
+    private async Task DispatchHoldings()
+    {
+        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Holdings", Data.ChronoRunId);
+        var triggerHoldingsCommand =
+            new TriggerBackfillHoldingsCommand(Data.RebuildStartDate, Data.Tomorrow, Data.PipelineMode);
+        Data.HoldingsRunId = triggerHoldingsCommand.HoldingsRunId;
+        await mediator.SendAsync(triggerHoldingsCommand);
+    }
+
+    private async Task DispatchPrices()
+    {
+        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Prices", Data.ChronoRunId);
+        var triggerPricesCommand =
+            new TriggerRefreshPricesCommand(null, Data.RebuildStartDate, Data.Today, Data.PipelineMode);
+        Data.PricesRunId = triggerPricesCommand.PricesRunId;
+        await mediator.SendAsync(triggerPricesCommand);
+    }
+
+    private async Task DispatchPerformance()
+    {
+        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Performance", Data.ChronoRunId);
+        var triggerPerformanceCommand = new TriggerCalculatePerformanceCommand(null, Data.PipelineMode);
+        Data.PerformanceRunId = triggerPerformanceCommand.PerformanceRunId;
+        await mediator.SendAsync(triggerPerformanceCommand);
+    }
+
+    private async Task DispatchPositions()
+    {
+        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Positions", Data.ChronoRunId);
+        var positionsCommand = new TriggerCalculatePositionsCommand(Data.PipelineMode);
+        Data.PositionsRunId = positionsCommand.PositionsRunId;
+        await mediator.SendAsync(positionsCommand);
+    }
+
+    private async Task DispatchSnapshots()
+    {
+        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Snapshots", Data.ChronoRunId);
+        var triggerSnapshotsCommand = new TriggerCalculateSnapshotsCommand(Data.Today.Year, Data.PipelineMode);
+        Data.SnapshotsRunId = triggerSnapshotsCommand.SnapshotsRunId;
+        await mediator.SendAsync(triggerSnapshotsCommand);
+    }
+
+    private async Task DispatchValuations()
+    {
+        logger.LogInformation("CloseOfDay {ChronoRunId}: Dispatching Valuations", Data.ChronoRunId);
+        var triggerValuationsCommand =
+            new TriggerBackfillValuationsCommand(null, Data.RebuildStartDate, Data.Tomorrow, Data.PipelineMode);
+        Data.ValuationsRunId = triggerValuationsCommand.ValuationsRunId;
+        await mediator.SendAsync(triggerValuationsCommand);
+    }
+
     private void CompleteSaga()
     {
         MarkAsComplete();
