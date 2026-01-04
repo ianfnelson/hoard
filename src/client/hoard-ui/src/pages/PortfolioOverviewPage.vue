be568a1c957576afddd5359c3018b399cfa4491a
Ian Nelson <ian@iannelson.uk>
2026-01-04 16:17:41 +0000

    feat: portfolio overview ui tweaks

--- src/client/hoard-ui/src/pages/PortfolioOverviewPage.vue
+++ src/client/hoard-ui/src/pages/PortfolioOverviewPage.vue
@@ -28,9 +28,14 @@ watch(
 
 const header_data = [
   { title: "Ticker", key: "instrumentTicker" },
-  { title: "Name", key: "instrumentName"},
-  { title: "Value", key: "value" },
-  { title: "%", key: "portfolioPercentage"}
+  { title: "Value", key: "value", align: 'end' },
+  { title: "Change", key: "valueChange", align: 'end'},
+  { title: "1D", key: "return1D", align: 'end'},
+  { title: "1Y", key: "return1Y", align: 'end'},
+  { title: "Profit", key: "profit", align: 'end'},
+  { title: "All", key: "returnAllTime", align: 'end'},
+  { title: "Annual", key: "annualisedReturn", align: 'end'},
+  { title: "%", key: "portfolioPercentage", align: 'end'}
 ] as const;
 
 // Shape rows for the table (keep template dumb)
@@ -38,32 +43,65 @@ const rows = computed(() =>
   store.openPositions.map(p => ({
     instrumentTicker: p.instrumentTicker,
     instrumentName: p.instrumentName,
-    value: p.performance?.value ?? 0,
-    portfolioPercentage: p.portfolioPercentage
+    value: p.performance?.value,
+    valueChange: p.performance?.valueChange,
+    return1D: p.performance?.return1D,
+    return1Y: p.performance?.return1Y,
+    returnAllTime: p.performance?.returnAllTime,
+    annualisedReturn: p.performance?.annualisedReturn,
+    portfolioPercentage: p.portfolioPercentage,
+    profit: p.performance ? p.performance?.unrealisedGain + p.performance?.realisedGain + p.performance?.income : null
   }))
 );
 </script>
 
 <template>
   <v-container>
-    <v-card>
+    <v-card :loading="store.isLoading">
       <v-card-title>
-        Portfolio positions
+        {{ store.portfolio?.name || '' }}
       </v-card-title>
 
       <v-card-text>
         <v-data-table
           :headers="header_data"
           :items="rows"
+          :items-per-page="-1"
           :loading="store.isLoading"
           density="compact"
+          :sort-by="[{ key: 'value', order: 'desc' }]"
+          hide-default-footer
         >
           <template #item.value="{ value }">
-            {{ value.toFixed(2) }}
+            {{ value.toLocaleString('en-GB', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}
+          </template>
+
+          <template #item.valueChange="{ value }">
+            {{ value.toLocaleString('en-GB', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}
+          </template>
+
+          <template #item.return1D="{ value }">
+            {{ value.toFixed(2) || '' }}
+          </template>
+
+          <template #item.return1Y="{ value }">
+            {{ value?.toFixed(2) || '' }}
+          </template>
+
+          <template #item.profit="{ value }">
+            {{ value.toLocaleString('en-GB', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }}
+          </template>
+
+          <template #item.returnAllTime="{ value }">
+            {{ value?.toFixed(2) || '' }}
+          </template>
+
+          <template #item.annualisedReturn="{ value }">
+            {{ value?.toFixed(2) || '' }}
           </template>
 
           <template #item.portfolioPercentage="{ value }">
-            {{ value.toFixed(2) }}%
+            {{ value.toFixed(2) }}
           </template>
 
           <template #no-data>
