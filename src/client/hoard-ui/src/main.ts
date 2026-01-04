1fc2a2714e300cdf212002b91743f230c0c3c0c9
Ian Nelson <ian@loxodromic.systems>
2026-01-04 16:32:21 +0000

    feat: portfolio overview ui tweaks (#279)

--- src/client/hoard-ui/src/main.ts
+++ src/client/hoard-ui/src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'vuetify/styles/main.css'
+import 'vuetify/lib/styles/main.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 import router from "./router";
 import App from './App.vue'
@@ -11,6 +12,13 @@ import App from './App.vue'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets : {
+      mdi
+    }
+  }
 })
 
 createApp(App)
