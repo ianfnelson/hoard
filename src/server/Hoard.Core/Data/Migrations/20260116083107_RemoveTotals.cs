ac9402fd5cb7eaa699ed548ac57d5960cbbf7ca0
Ian Nelson <ian@loxodromic.systems>
2026-01-16 08:33:02 +0000

    fix: missing EF migration (#294)

---
+++ src/server/Hoard.Core/Data/Migrations/20260116083107_RemoveTotals.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hoard.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTotals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalDeposits",
+                table: "PortfolioSnapshot");
+
+            migrationBuilder.DropColumn(
+                name: "TotalIncome",
+                table: "PortfolioSnapshot");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalDeposits",
+                table: "PortfolioSnapshot",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalIncome",
+                table: "PortfolioSnapshot",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
